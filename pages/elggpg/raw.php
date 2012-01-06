014f6924b0946500c723595161d65a210d3fc6c8
Sem <sembrestels@riseup.net>
2012-01-06 09:04:19 +0100

    Refactored raw page.

--- pages/elggpg/raw.php
+++ pages/elggpg/raw.php
@@ -1,12 +1,17 @@
 <?php
-  header("Content-type: text/plain");
-  if (!isloggedin()) {
-    forward();
-  }
+/**
+ * GPG Public Key download
+ *
+ * @package ElggPG
+ */
+  
+if (!elgg_is_logged_in()) {
+	forward();
+}
 
-  putenv("GNUPGHOME=".elggpg_get_gpg_home());
-  $gnupg = new gnupg();
-	$user = get_user_by_username(get_input('username'));
+putenv("GNUPGHOME=" . elggpg_get_gpg_home());
+$gnupg = new gnupg();
 
-	echo $gnupg->export($user->openpgp_publickey);
-?>
+header("Content-type: text/plain");
+$user = get_user_by_username(get_input('username'));
+echo $gnupg->export($user->openpgp_publickey);
