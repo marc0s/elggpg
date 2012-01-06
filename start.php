2948691c18312dc853f8bcf1dfc5d5bb76c4e6d1
Sem <sembrestels@riseup.net>
2012-01-06 10:46:59 +0100

    Upgraded send encrypted emails function.

--- start.php
+++ start.php
@@ -22,7 +22,7 @@ function elggpg_init() {
 	elgg_register_entity_type('object', 'elggpg');
 
 	// Register a notification handler to encrypt messages
-	register_notification_handler("email", "elggpg_email_notify_handler");
+	elgg_register_plugin_hook_handler('email', 'system', 'elggpg_send_email_handler');
 	
 	elgg_extend_view("core/settings/account/email", "elggpg/viewkey", 1);
 	
@@ -220,99 +220,68 @@ function elggpg_messages_send($subject, $body, $send_to, $from = 0, $reply = 0, 
 
 }
 
-function elggpg_email_notify_handler(ElggEntity $from, ElggUser $to, $subject, $message, array $params = NULL) {
-        global $CONFIG;
-
-        if (!$from) {
-                throw new NotificationException(sprintf(elgg_echo('NotificationException:MissingParameter'), 'from'));
-        }
-
-        if (!$to) {
-                throw new NotificationException(sprintf(elgg_echo('NotificationException:MissingParameter'), 'to'));
-        }
-
-        if ($to->email=="") {
-                throw new NotificationException(sprintf(elgg_echo('NotificationException:NoEmailAddress'), $to->guid));
-        }
+function elggpg_send_email_handler($hook, $type, $return, $params) {
+	$from = $params['from'];
+	$to = $params['to'];
+	$subject = $params['subject'];
+	$body = $params['body'];
 	
-        // Sanitise subject
-        $subject = preg_replace("/(\r\n|\r|\n)/", " ", $subject); // Strip line endings
-        // To
-        $to = $to->email;
+	$receiver = current(get_user_by_email($to));
+	
+	// Format message
+	$body = html_entity_decode($body, ENT_COMPAT, 'UTF-8'); // Decode any html entities
+	$body = elgg_strip_tags($body); // Strip tags from message
+	$body = preg_replace("/(\r\n|\r)/", "\n", $body); // Convert to unix line endings in body
+	$body = preg_replace("/^From/", ">From", $body); // Change lines starting with From to >From
+	$body = wordwrap($body);
+	
+	// Trying to encrypt
+	try {
+		if (strpos($body, "-----BEGIN PGP MESSAGE-----") === false) {
+			putenv("GNUPGHOME=".elggpg_get_gpg_home());
+			$gpg = new gnupg();
+			$gpg->addencryptkey($receiver->openpgp_publickey);
+			if ($encrbody = $gpg->encrypt($body)) {
+				$body = $encrbody;
+			}
+		}
+	} catch (Exception $e) {
+	}
 
-        // From
-        $site = get_entity($CONFIG->site_guid);
-        // If there's an email address, use it - but only if its not from a user.
-        if ((isset($from->email)) && (!($from instanceof ElggUser))) {
-                $from = $from->email;
-        } else if (($site) && (isset($site->email))) {
-                // Has the current site got a from email address?
-                $from = $site->email;
-        } else if (isset($from->url))  {
-                // If we have a url then try and use that.
-                $breakdown = parse_url($from->url);
-                $from = 'noreply@' . $breakdown['host']; // Handle anything with a url
-        } else {
-                // If all else fails, use the domain of the site.
-                $from = 'noreply@' . get_site_domain($CONFIG->site_guid);
-        }
+	// The following code is the same that in elgg's
+	
+	$header_eol = "\r\n";
+	if (isset($CONFIG->broken_mta) && $CONFIG->broken_mta) {
+		// Allow non-RFC 2822 mail headers to support some broken MTAs
+		$header_eol = "\n";
+	}
 
-        if (is_callable('mb_internal_encoding')) {
-                mb_internal_encoding('UTF-8');
-        }
-        $site = get_entity($CONFIG->site_guid);
-        $sitename = $site->name;
-        if (is_callable('mb_encode_mimeheader')) {
-                $sitename = mb_encode_mimeheader($site->name,"UTF-8", "B");
-        }
+	// Windows is somewhat broken, so we use just address for to and from
+	if (strtolower(substr(PHP_OS, 0, 3)) == 'win') {
+		// strip name from to and from
+		if (strpos($to, '<')) {
+			preg_match('/<(.*)>/', $to, $matches);
+			$to = $matches[1];
+		}
+		if (strpos($from, '<')) {
+			preg_match('/<(.*)>/', $from, $matches);
+			$from = $matches[1];
+		}
+	}
 
-        $header_eol = "\r\n";
-        if (
-                (isset($CONFIG->broken_mta)) &&
-                ($CONFIG->broken_mta)
-        ) {
-                // Allow non-RFC 2822 mail headers to support some broken MTAs
-                $header_eol = "\n";
-        }
+	$headers = "From: $from{$header_eol}"
+		. "Content-Type: text/plain; charset=UTF-8; format=flowed{$header_eol}"
+		. "MIME-Version: 1.0{$header_eol}"
+		. "Content-Transfer-Encoding: 8bit{$header_eol}";
 
-        $from_email = "\"$sitename\" <$from>";
-        if (strtolower(substr(PHP_OS, 0 , 3)) == 'win') {
-                // Windows is somewhat broken, so we use a different format from header
-                $from_email = "$from";
-        }
 
-        $headers = "From: $from_email{$header_eol}"
-                . "Content-Type: text/plain; charset=UTF-8; format=flowed{$header_eol}"
-                . "MIME-Version: 1.0{$header_eol}"
-                . "Content-Transfer-Encoding: 8bit{$header_eol}";
+	// Sanitise subject by stripping line endings
+	$subject = preg_replace("/(\r\n|\r|\n)/", " ", $subject);
+	if (is_callable('mb_encode_mimeheader')) {
+		$subject = mb_encode_mimeheader($subject, "UTF-8", "B");
+	}
 
-        if (is_callable('mb_encode_mimeheader')) {
-                $subject = mb_encode_mimeheader($subject,"UTF-8", "B");
-        }
-
-        // Format message
-        $message = html_entity_decode($message, ENT_COMPAT, 'UTF-8'); // Decode any html entities
-        $message = strip_tags($message); // Strip tags from message
-        $message = preg_replace("/(\r\n|\r)/", "\n", $message); // Convert to unix line endings in body
-        $message = preg_replace("/^From/", ">From", $message); // Change lines starting with From to >From
-        $message = wordwrap($message);
-        try {
-            if (strpos($message, "-----BEGIN PGP MESSAGE-----") === false) {
-                putenv("GNUPGHOME=".elggpg_get_gpg_home());
-                $gpg = new gnupg();
-                $gpg->addencryptkey($to);
-                //$gpg = new Crypt_GPG(array('homedir' => elggpg_get_gpg_home()));
-                //$gpg->addEncryptKey($to);
-                $encmessage = $gpg->encrypt($message);
-		if ($encmessage)
-			$message = $encmessage;
-            } else {
-            }
-        }
-        catch (Exception $e) {
-        }
-
-        return mail($to, $subject, $message, $headers);
+	return mail($to, $subject, $body, $headers);
 }
 
 /**
