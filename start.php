89b8ce3f3d4cb5c4c3764e05fe4b067f1b7af416
Sem <sembrestels@riseup.net>
2012-01-06 04:34:28 +0100

    Upgraded start to new Elgg's code practices.

--- start.php
+++ start.php
@@ -1,83 +1,77 @@
 <?php
-        /**
-         * Elgg gpg management
-         * 
-         * @package ElggGpg
-         * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-         * @author Pablo Martin
-         * @copyright Pablo Martin
-         */
+/**
+ * Elgg GnuPG plugin
+ *
+ * @package ElggPG
+ */
 
+elgg_register_event_handler('init', 'system', 'elggpg_init');
 
 /**
- * Get the gpg home folder
-*/
-function elggpg_get_gpg_home()
-{
-	global $CONFIG;
-	// try to find location of settings from environment file,
-	// which means the gpg directory goes at the same level.
-	$elgg_config = getenv("elgg_config");
-	if ($elgg_config && is_dir(dirname($elgg_config)."/gpg"))
-		return dirname($elgg_config)."/gpg";
-	// otherwise create a gpg folder at the data folder
-	// and store the keys there
-	$gpg_path = $CONFIG->dataroot . "gpg";
-	if (!file_exists($gpg_path))
-		mkdir($gpg_path);
-	return $gpg_path;
-}
+ * GnuPG plugin initialization functions.
+ */
+function elggpg_init() {
+	
+	// Extend CSS
+	elgg_extend_view('css/elgg', 'elggpg/css');
+	
+	// Register a page handler, so we can have nice URLs
+	elgg_register_page_handler('elggpg', 'elggpg_page_handler');
+	
+	// Register entity type for search
+	elgg_register_entity_type('object', 'elggpg');
 
+	// Register a notification handler to encrypt messages
+	register_notification_handler("email", "elggpg_email_notify_handler");
+	
+	elgg_extend_view("core/settings/account/email", "elggpg/viewkey", 1);
+	
+	// Actions
+	$actions_path = elgg_get_plugins_path() . 'elggpg/actions/elggpg';
+	elgg_register_action("elggpg/pub_key_upload", "$actions_path/pub_key_upload.php");
+	elgg_register_action("elggpg/send", "actions_path/send_encrypted.php");
+
+	// add a GPG link to owner blocks
+	elgg_register_plugin_hook_handler('register', 'menu:owner_block', 'elggpg_owner_block_menu');
+
+}
 
 /**
  * Page handler
 */
-function elggpg_handler($page) {
-	global $CONFIG;
-	$pages = dirname(__FILE__) . '/pages/';
+function elggpg_page_handler($page) {
+
+	$pages_dir = elgg_get_plugins_path() . 'elggpg/pages/elggpg';
 	switch($page[0]) {
 		case 'owner':
-			include("$pages/owner.php");
+			include("$pages_dir/owner.php");
 			break;
 		case 'raw':
 			set_input('username', $page[1]);
-			include("$pages/raw.php");
+			include("$pages_dir/raw.php");
 			break;
 		default:
-      echo "empty door";
-		//	set_input('user',get_user_by_username($page[0])->getGUID());
-		//	include("$pages/show.php");
+			return false;
 	}
+	return true;
 }
 
-/**
- * Init engine hooks
-*/
-function elggpg_init() {
-	global $CONFIG;
-
-	// Page handler
-	include($CONFIG->pluginspath . "elggpg/mailhandler.php");
-	register_page_handler('elggpg','elggpg_handler');
-
-	// Register entity type for search
-	elgg_register_entity_type('object', 'elggpg');
-
-
-	// Keys
-	register_notification_handler("email", "elggpg_email_notify_handler");
-	register_action("elggpg/pub_key_upload",false,$CONFIG->pluginspath . "elggpg/actions/pub_key_upload.php");
-	register_action("elggpg/send",false,$CONFIG->pluginspath . "elggpg/actions/send_encrypted.php");
-
-	// Extend profile menu   
-	elgg_register_plugin_hook_handler('register', 'menu:owner_block', 'elggpg_owner_block_menu');
-
-	elgg_extend_view('css/elgg','elggpg/css');
-  elgg_extend_view("core/settings/account/email", "elggpg/viewkey", 1);
-
+function elggpg_get_gpg_home() {
+	// try to find location of settings from environment file,
+	// which means the gpg directory goes at the same level.
+	$elgg_config = getenv("elgg_config");
+	if ($elgg_config && is_dir(dirname($elgg_config)."/gpg")) {
+		return dirname($elgg_config)."/gpg";
+	}
+	
+	// otherwise create a gpg folder at the data folder
+	// and store the keys there
+	$gpg_path = elgg_get_data_path() . "gpg/";
+	if (!file_exists($gpg_path)) {
+		mkdir($gpg_path);
+	}
+	return $gpg_path;
 }
-
-register_elgg_event_handler('init','system','elggpg_init');
 
 /**
  * Send an internal message. "Fork" from the same function under the messages module. This one can save
@@ -225,6 +219,101 @@ function elggpg_messages_send($subject, $body, $send_to, $from = 0, $reply = 0, 
 
 }
 
+function elggpg_email_notify_handler(ElggEntity $from, ElggUser $to, $subject, $message, array $params = NULL) {
+        global $CONFIG;
+
+        if (!$from) {
+                throw new NotificationException(sprintf(elgg_echo('NotificationException:MissingParameter'), 'from'));
+        }
+
+        if (!$to) {
+                throw new NotificationException(sprintf(elgg_echo('NotificationException:MissingParameter'), 'to'));
+        }
+
+        if ($to->email=="") {
+                throw new NotificationException(sprintf(elgg_echo('NotificationException:NoEmailAddress'), $to->guid));
+        }
+	
+        // Sanitise subject
+        $subject = preg_replace("/(\r\n|\r|\n)/", " ", $subject); // Strip line endings
+        // To
+        $to = $to->email;
+
+        // From
+        $site = get_entity($CONFIG->site_guid);
+        // If there's an email address, use it - but only if its not from a user.
+        if ((isset($from->email)) && (!($from instanceof ElggUser))) {
+                $from = $from->email;
+        } else if (($site) && (isset($site->email))) {
+                // Has the current site got a from email address?
+                $from = $site->email;
+        } else if (isset($from->url))  {
+                // If we have a url then try and use that.
+                $breakdown = parse_url($from->url);
+                $from = 'noreply@' . $breakdown['host']; // Handle anything with a url
+        } else {
+                // If all else fails, use the domain of the site.
+                $from = 'noreply@' . get_site_domain($CONFIG->site_guid);
+        }
+
+        if (is_callable('mb_internal_encoding')) {
+                mb_internal_encoding('UTF-8');
+        }
+        $site = get_entity($CONFIG->site_guid);
+        $sitename = $site->name;
+        if (is_callable('mb_encode_mimeheader')) {
+                $sitename = mb_encode_mimeheader($site->name,"UTF-8", "B");
+        }
+
+        $header_eol = "\r\n";
+        if (
+                (isset($CONFIG->broken_mta)) &&
+                ($CONFIG->broken_mta)
+        ) {
+                // Allow non-RFC 2822 mail headers to support some broken MTAs
+                $header_eol = "\n";
+        }
+
+        $from_email = "\"$sitename\" <$from>";
+        if (strtolower(substr(PHP_OS, 0 , 3)) == 'win') {
+                // Windows is somewhat broken, so we use a different format from header
+                $from_email = "$from";
+        }
+
+        $headers = "From: $from_email{$header_eol}"
+                . "Content-Type: text/plain; charset=UTF-8; format=flowed{$header_eol}"
+                . "MIME-Version: 1.0{$header_eol}"
+                . "Content-Transfer-Encoding: 8bit{$header_eol}";
+
+        if (is_callable('mb_encode_mimeheader')) {
+                $subject = mb_encode_mimeheader($subject,"UTF-8", "B");
+        }
+
+        // Format message
+        $message = html_entity_decode($message, ENT_COMPAT, 'UTF-8'); // Decode any html entities
+        $message = strip_tags($message); // Strip tags from message
+        $message = preg_replace("/(\r\n|\r)/", "\n", $message); // Convert to unix line endings in body
+        $message = preg_replace("/^From/", ">From", $message); // Change lines starting with From to >From
+        $message = wordwrap($message);
+        try {
+            if (strpos($message, "-----BEGIN PGP MESSAGE-----") === false) {
+                putenv("GNUPGHOME=".elggpg_get_gpg_home());
+                $gpg = new gnupg();
+                $gpg->addencryptkey($to);
+                //$gpg = new Crypt_GPG(array('homedir' => elggpg_get_gpg_home()));
+                //$gpg->addEncryptKey($to);
+                $encmessage = $gpg->encrypt($message);
+		if ($encmessage)
+			$message = $encmessage;
+            } else {
+            }
+        }
+        catch (Exception $e) {
+        }
+
+        return mail($to, $subject, $message, $headers);
+}
+
 /**
  * Add a menu item to an ownerblock
  * 
@@ -236,19 +325,12 @@ function elggpg_messages_send($subject, $body, $send_to, $from = 0, $reply = 0, 
 function elggpg_owner_block_menu($hook, $type, $return, $params) {
 	if (elgg_instanceof($params['entity'], 'user')) {
 		$url = "elggpg/owner/{$params['entity']->username}";
-    if ($params['entity'] == elgg_get_logged_in_user_entity()) 
-    {
-		  $item = new ElggMenuItem('elggpg', elgg_echo('elggpg:manage'), $url);
-    } 
-    else 
-    {
-		  $item = new ElggMenuItem('elggpg', elgg_echo('elggpg:view'), $url);
-    }
+		if ($params['entity'] == elgg_get_logged_in_user_entity()) {
+			$item = new ElggMenuItem('elggpg', elgg_echo('elggpg:manage'), $url);
+		} else {
+			$item = new ElggMenuItem('elggpg', elgg_echo('elggpg:view'), $url);
+		}
 		$return[] = $item;
-	} 
-
+	}
 	return $return;
 }
-
-
-?>
