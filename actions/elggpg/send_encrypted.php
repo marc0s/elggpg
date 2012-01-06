b7a9cd3316e55e2640dc86906d12ec335a0772f2
Sem <sembrestels@riseup.net>
2012-01-06 11:29:16 +0100

    Added send encrypted private message action. See comments in the code to know issues.

--- actions/elggpg/send_encrypted.php
+++ actions/elggpg/send_encrypted.php
@@ -1,84 +1,74 @@
 <?php
+/**
+* Ssend a message action
+* 
+* @package ElggMessages
+*/
 
-	/**
-	 * Elgg send a message action page
-	 * 
-	 * @package ElggMessages
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Curverider Ltd <info@elgg.com>
-	 * @copyright Curverider Ltd 2008-2010
-	 * @link http://elgg.com/
-	 */
-	 
-	 // Make sure we're logged in (send us to the front page if not)
-		if (!isloggedin()) forward();
+$subject = strip_tags(get_input('subject'));
+$body = get_input('body');
+$recipient_guid = get_input('recipient_guid');
 
-		putenv("GNUPGHOME=".elggpg_get_gpg_home());
-		$gpg = new gnupg();
+elgg_make_sticky_form('messages');
 
-	  // Get input data
-		$title = get_input('title'); // message title
-		$message_contents = get_input('message'); // the message
-    $forward_url = "messages/inbox/".$_SESSION['user']->username;
+//$reply = get_input('reply',0); // this is the guid of the message replying to
 
-		$send_to = get_input('send_to'); // this is the user guid to whom the message is going to be sent
-		$reply = get_input('reply',0); // this is the guid of the message replying to
-		try {
-			$gpg->addencryptkey(get_loggedin_user()->openpgp_publickey);
-			$message_contents_from = $gpg->encrypt(strip_tags($message_contents));
-		}
-		catch (Exception $e) {
-			$message_contents_from = $message_contents;
-		}
-		
-		$gpg->cleardecryptkeys();
-		$gpg->addencryptkey(get_entity($send_to)->openpgp_publickey);
-		$message_contents = $gpg->encrypt(strip_tags($message_contents));
+if (!$recipient_guid) {
+	register_error(elgg_echo("messages:user:blank"));
+	forward("messages/compose");
+}
 
-	  // Cache to the session to make form sticky
-		$_SESSION['msg_to'] = $send_to;
-		
-		if($title == ' '){
-			$title = elgg_echo("elggpg:nosubject");
-		}
-		$_SESSION['msg_title'] = $title;
-		$_SESSION['msg_contents'] = $message_contents;
+$user = get_user($recipient_guid);
+if (!$user) {
+	register_error(elgg_echo("messages:user:nonexist"));
+	forward("messages/compose");
+}
 
-		if (empty($send_to)) {
-			register_error(elgg_echo("messages:user:blank"));
-			forward($forward_url);
-		}
-		
-		$user = get_user($send_to);
-		if (!$user) {
-			register_error(elgg_echo("messages:user:nonexist"));
-			forward($forward_url);
-		}
+// Make sure the message field, send to field and title are not blank
+if (!$body || !$subject) {
+	register_error(elgg_echo("messages:blank"));
+	forward("messages/compose");
+}
 
-	// Make sure the message field, send to field and title are not blank
-		if (empty($message_contents) || empty($title)) {
-			register_error(elgg_echo("messages:blank"));
-			forward($forward_url);
-		}
-		
-	// Otherwise, 'send' the message 
-	$result = elggpg_messages_send($title,$message_contents,$send_to,$send_to,$reply,true,false,true);
-			
-	// Save 'send' the message
-		if (!$result) {
-			register_error(elgg_echo("messages:error"));
-			forward($forward_url);
-		}
+// Otherwise, encrypt and 'send' the message 
 
-	// successful so uncache form values
-		unset($_SESSION['msg_to']);
-		unset($_SESSION['msg_title']);
-		unset($_SESSION['msg_contents']);
-			
-	// Success message
-		system_message(elgg_echo("messages:posted"));
+putenv("GNUPGHOME=".elggpg_get_gpg_home());
+$gpg = new gnupg();
+
+try {
+	$gpg->addencryptkey(elgg_get_logged_in_user_entity()->openpgp_publickey);
+	if ($encrbody = $gpg->encrypt(strip_tags($body))) {
+		$body_from = $encrbody;
+	}
+} catch (Exception $e) {
+}
+$body_from = $body;
 	
-	// Forward to the users inbox
-		forward($forward_url);
+$gpg->cleardecryptkeys();
 
-?>
+// FIXME Encrypting do not works
+try {
+	$gpg->addencryptkey($user->openpgp_publickey);
+	if ($encrbody = $gpg->encrypt(strip_tags($body))) {
+		$body_to = $encrbody;
+	}
+} catch (Exception $e) {
+}
+$body_to = $body;
+
+// TODO: messages_send saves two copies of the message. In previous version of the
+// elggpg plugin, this function was overriden to allow save both versions encrypted
+// with its owner key. Nowadays there is only one version saved, the 'sent' one.
+$result = messages_send($subject, $body_to, $recipient_guid, 0, $reply);
+
+// Save 'send' the message
+if (!$result) {
+	register_error(elgg_echo("messages:error"));
+	forward("messages/compose");
+}
+
+elgg_clear_sticky_form('messages');
+	
+system_message(elgg_echo("messages:posted"));
+
+forward('messages/inbox/' . elgg_get_logged_in_user_entity()->username);
